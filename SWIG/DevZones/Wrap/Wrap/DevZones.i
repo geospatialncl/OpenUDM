ca02f5e0c4d76857650013030c245591be1b763a
Tom Russell <tomalrussell@gmail.com>
2020-02-19 17:32:40 +0000

    Move files up to root, drop SWIG projects
    Adds DevZones, MultiCriteriaEval and RasterToolkit

--- SWIG/DevZones/Wrap/Wrap/DevZones.i
+++
@@ -1,9 +0,0 @@
-%module DevZones
-%include "std_string.i"
-
-%{
-#include "DevZones.h"
-%}
-
-%include "DevZones.h"
-
--- SWIG/MultiCriteriaEval/Wrap/Wrap/MultiCriteriaEval.h
+++
@@ -1,9 +0,0 @@
-#include "DRaster.h"
-#include "IRaster.h"
-#include "CSVToolkit.h"
-#include <chrono>
-
-using namespace std;
-
-void MaskedWeightedSum(bool useBin, std::string iRasCount, std::string iRasInputs, std::string dRasCount, std::string dRasInputs, std::string output,
-						std::string rastHdr, std::string binConfigPath, bool reverse);
