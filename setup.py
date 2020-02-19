6ac28eb1945ab87ad2acc10f46f674c1070c2dfe
Tom Russell <tomalrussell@gmail.com>
2020-02-19 17:34:45 +0000

    Build all swig modules through setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,24 @@
 from setuptools import Extension, setup
+from glob import glob
 
-model = Extension(
-    '_CellularModel',
-    sources=[
-        'CellularModel.i',
-        'CellularModel.cpp',
-        'CSVToolkit.cpp',
-        'DRaster.cpp',
-        'IRaster.cpp',
-        'UDMCell.cpp',
-        'UDMWard.cpp',
-        'UDMZone.cpp'
-    ],
-    swig_opts=['-c++']
-)
+sources = glob('*.cpp') + glob('*.i')
+
+py_modules = ['CellularModel', 'RasterToolkit', 'MultiCriteriaEval', 'DevZones']
+
+ext_modules = [
+    Extension(
+        f"_{name}",
+        sources=sources,
+        swig_opts=['-c++']
+    )
+    for name in py_modules
+]
 
 setup(
     name='openudm',
     version='1.0.0',
     author='James Virgo',
     description='Urban Development Model',
-    ext_modules=[model],
-    py_modules=["CellularModel"]
+    ext_modules=ext_modules,
+    py_modules=py_modules
 )
