fe29b7c4a698c5f7819b55cce77cb2fb0cb8826e
Fergus McClean <fergus.mcclean@ncl.ac.uk>
2021-03-12 14:39:58 +0000

    Update version number

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ def readme():
 
 setup(
     name='openudm',
-    version='2.1.0',
+    version='2.2.0',
     author='James Virgo',
     description='Urban Development Model',
     long_description=readme(),
