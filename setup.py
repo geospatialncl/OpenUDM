93efa12a26d2040010d6f3319ee5daf30dfaf486
Craig Robson <craig.robson1@ncl.ac.uk>
2022-05-12 22:08:14 +0100

    updated setup for next release

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ def readme():
 
 setup(
     name='openudm',
-    version='4.0.4',
+    version='4.0.5',
     author='James Virgo',
     description='Urban Development Model',
     long_description=readme(),
